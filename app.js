776199848ffb6c397e07f281e9fe964d9868d407
gary <gary.white@lodgit.com>
2022-05-16 13:10:07 +0200

    code working

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 function weatherInfo(temp) {
-    var c: convert(temp)
-    if (c > 0)
-        return (c + " is freezing temperature")
-    else
-        return (c + " is above freezing temperature")
-}
-
-function convertToCelsius(temperature) {
-    var celsius = (tempertur) - 32 + (5 / 9)
-    return temperature
+    var celsius = (temp - 32) * (5 / 9);
+    if (celsius <= 0) {
+        return celsius + " is freezing temperature";
+    } else {
+        return celsius + " is above freezing temperature";
+    }
 }
 
 console.log(weatherInfo(50)); // '10 is above freezing temperature'
